aabaae09657c311f9a0eb06327750af7681dc4fb
Yogesh Prabhu <yogeshprabhu@hotmail.com>
2022-02-06 14:48:36 -0800

    Module 5. Add Data Validation to the Form

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -10,10 +10,15 @@ namespace BugTrackerUI
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(length:10)]
         public string Description { get; set; }
 
+        [Required]
+        [Range(minimum:1, maximum:5)]
         public int Priority { get; set; }
     }
 }
